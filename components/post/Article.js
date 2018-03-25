e4242c6712b8a2410efa6c037b3e16404116583e
Branden Kim <brandenk0@gmail.com>
2018-03-26 00:00:53 +0100

    Refactor post page to separate components

---
+++ components/post/Article.js
@@ -0,0 +1,66 @@
+
+function getDate(dateString) {
+  var d = new Date(dateString)
+  return '- ' + d.toLocaleDateString('en-us', {month: 'long'}) 
+         + ' ' + d.getDay() + ', ' + d.getFullYear()
+}
+
+const Article = ({title, featured, author, date, content}) => {
+  return (
+    <div id="article">
+      <div id="headline">
+        <h2>{title}</h2>
+        <div id="details">
+          <p>{author}</p>
+          <p>{getDate(date)}</p>
+        </div>
+        <img src={featured} />
+      </div>
+      <div id="content" 
+        dangerouslySetInnerHTML={{ __html: content }} />
+      
+      <style jsx>{`
+        #article {
+          display: flex;
+          flex-flow: column nowrap;
+          justify-content: center;
+          align-items: center;
+          max-width: 500px;
+          margin: 0 auto;
+          font-family: "Times New Roman";
+        }
+
+        #headline {
+          display: flex;
+          align-self: flex-start;
+          flex-flow: column wrap;
+          margin: 0 auto;
+          max-width: 500px;
+        }
+
+        #headline h2 {
+          width: 80%;
+        }
+
+        #details {
+          display: flex;
+          flex-flow: row wrap;
+        }
+
+        #details p {
+          font-family: Roboto, sans-serif;
+          font-size: 12px;
+          padding: 0;
+          margin: 0 5px 0 0;
+        }
+
+        img {
+          max-width: 100%;
+        }
+      `}
+      </style>
+    </div>
+  )
+}
+
+export default Article
--- pages/post.js
+++ pages/post.js
@@ -1,92 +1,72 @@
 import Header from '../components/header/Header'
+import Article from '../components/post/Article'
+import PostSideBar from '../components/post/PostSideBar'
+
+import Head from 'next/head'
+import React from 'react'
 
 const axios = require('axios')
 
 
 class Post extends React.Component {
 
-  constructor(props) {
-    super(props)
-
-    //console.log(props.url.query.slug)
+  static async getInitialProps() {
     const url = 'http://18.219.114.43/wp-json/wp/v2/posts/22'
+    const res = await axios.get(url)
+    const data = await res.data
 
-    axios.get(url)
-      .then(response => {
-        const data = response.data
-        
-        const mediaURL =
-          'http://18.219.114.43/wp-json/wp/v2/media/' + data.featured_media
-        const authorURL =
-          'http://18.219.114.43/wp-json/wp/v2/users/' + data.author
+    // Update URL Strings with post data
+    const mediaURL =
+      'http://18.219.114.43/wp-json/wp/v2/media/' + data.featured_media
+    const authorURL =
+      'http://18.219.114.43/wp-json/wp/v2/users/' + data.author
 
-        // GET Featured Media 
-        axios.get(mediaURL)
-          .then(response => {
-            const media = response.data
-            this.setState({ featured: media.description.rendered })
-          })
-          .catch(function (error) {
-            console.log(error)
-          })
+    // GET Featured Image 
+    const mediaRes = await axios.get(mediaURL)
+    const mediaData = await mediaRes.data
 
-        // GET Author Name
-        axios.get(authorURL)
-          .then(response => {
-            const author = response.data
-            this.setState({ author: author.name })
-          })
-          .catch(function (error) {
-            console.log(error)
-          })
-        
-        // Set new state to update the content of the post
-        this.setState({ content: data.content.rendered })
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
+    // GET Author name
+    const authorRes = await axios.get(authorURL)
+    const authorData = await authorRes.data
+
+    // Return props for component
+    return {
+      title: data.title.rendered,
+      featured: mediaData.guid.rendered,
+      author: authorData.name,
+      date: data.modified,
+      content: data.content.rendered
+    }
   }
 
   render() {
-    if (this.state !== null) {
-      return (
-        <div id="body">
+    return (
+      <div>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+          <title>{this.props.title}</title>
+
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" /> 
+        </Head>
+        <div>
           <Header />
           <div id="content">
-            <div id="featured" key={this.state.featured} dangerouslySetInnerHTML={{ __html: this.state.featured }} />
-            <div key={this.state.author} dangerouslySetInnerHTML={{ __html: this.state.author }} />
-            <div key={this.state.content} dangerouslySetInnerHTML={{ __html: this.state.content }} />
+            <Article title={this.props.title}
+                     author={this.props.author}
+                     featured={this.props.featured}
+                     date={this.props.date}
+                     content={this.props.content} />
+            <PostSideBar />
           </div>
-          <style jsx>{`
-            #content {
-              display: flex;
-              flex-flow: column wrap;
-              justify-content: center;
-              align-items: center;
-              width: 50%;
-              margin: 0 auto;
-            }
-
-            #featured img {
-              width: 100%;
-            }
-          `}
-          </style>
         </div>
-      )
-    }
-    else {
-      return (
-        <div id="body">
-          <Header />
-          <style jsx>{`
-          `}
-          </style>
-        </div>
-      )
-    }
-
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
+      </div>
+    )
   }
 }
 
