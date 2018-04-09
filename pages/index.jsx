275cf1adf855e585df300ca7b7965842cd94e37e
Branden Kim <brandenk0@gmail.com>
2018-04-09 22:51:23 +0100

    Add border for front-page

--- pages/index.jsx
+++ pages/index.jsx
@@ -33,7 +33,7 @@ class Page extends React.Component {
         <Header />
         <div className="container" id="content">
           <div className="row">
-            <div className="col-12 col-md-6">
+            <div className="col-12 col-md-6 border-right border-left border-dark" id="center">
               <MainContent />
             </div>
             <div className="col-12 col-md-3 order-md-first" id="left">
@@ -44,11 +44,7 @@ class Page extends React.Component {
             </div>
           </div>
         </div>
-        <div className="footer" id="footer"><Footer /></div>
-
-        <style jsx>{`
-        `}
-        </style>
+        <Footer />
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
