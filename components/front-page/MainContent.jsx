80d45fd4c8347a85bd5ad84329fa9e93ee0ace84
Branden Kim <brandenk0@gmail.com>
2018-06-19 22:24:06 -0700

    Update to support new thumbnail components

--- components/front-page/MainContent.jsx
+++ components/front-page/MainContent.jsx
@@ -1,6 +1,6 @@
 // import components
-import Featured from '../thumbnail/Featured'
-import MediumNoSummary from '../thumbnail/MediumNoSummary'
+import LargeImage from '../thumbnail/LargeImage'
+import SideImage from '../thumbnail/SideImage'
 
 // import actions & 3rd party libraries
 import { fetchFeatured } from "../../actions"
@@ -9,18 +9,24 @@ import { connect } from "react-redux"
 // connect component to store
 @connect(store => {
   var featured = []
-  var otherArticles = []
+  var topStories = []
   if (store.featuredArticle.featured.length > 1) {
-    featured = store.featuredArticle.featured.slice(0, 1),
-    otherArticles = store.featuredArticle.featured.slice(1,store.featuredArticle.featured.length) 
+    featured = store.featuredArticle.featured.slice(0, 1)
+
+    const numArticles = 
+      store.featuredArticle.featured.length > 6
+        ? 6
+        : store.featuredArticle.featured.length
+    // Update w/ actual slice when articles are put in
+    topStories = store.featuredArticle.featured.slice(0, numArticles) 
   }
   else {
     featured = store.featuredArticle.featured.slice(0, 1)
-    otherArticles = []
+    topStories = []
   }
   return {
     featured: featured,
-    otherArticles: otherArticles,
+    topStories: topStories,
   }
   
 }, { fetchFeatured })
@@ -35,26 +41,54 @@ class MainContent extends React.Component {
 
   render() {
     const featured = this.props.featured ? this.props.featured : []
-    const otherArticles = this.props.otherArticles ? this.props.otherArticles : []
-
-    console.log(featured)
+    const topStories = this.props.topStories ? this.props.topStories : []
 
     return (
       <div className="container">
+        {/* Featured Headline with LargeImage */}
         <ul>
           {featured.map(featured => (
-            <Featured key={featured.slug} article={featured} />
+            <LargeImage key={featured.slug} article={featured} withSummary={true} />
           ))}
-          {/*
-          {otherArticles.map(article => (
-            <MediumNoSummary key={article.slug} article={article} />
-          ))}
-        */}
         </ul>
+        {/* List for Top Stories with LargeImage */}
+        {/* Limited to two articles */}
+        <div className="container" id="top-stories-large">
+          <div className="row">
+            {topStories.map(article => (
+              <div className="col-12 col-md-6" id="no-sum-large">
+                <ul>
+                  <LargeImage key={article.slug} article={article} withSummary={false} />
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* List for Featured Stories with SideImage */}
+        <div className="container" id="side-image-stories">
+          {topStories.map(article => (
+            <div className="row">
+              <ul>
+                <SideImage key={article.slug} article={article} withSummary={true} />
+              </ul>
+            </div>
+          ))}   
+        </div>
         <style jsx>{`
           ul {
             list-style-type: none;
             padding: 0;
+            margin: 0;
+          }
+          #top-stories-large{
+            margin-top: 2em;
+          }
+          #no-sum-large {
+            padding: 0;
+            margin: 0;
+          }
+          #side-image-stories {
+            margin-top: 1em;
           }
         `}
         </style>
