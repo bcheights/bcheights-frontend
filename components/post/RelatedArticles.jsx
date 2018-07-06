dee65b6c82a713d150c225555e4eff43f64b0674
Branden Kim <brandenk0@gmail.com>
2018-07-06 00:25:14 -0700

    Add base implementation for related articles field

--- components/post/RelatedArticles.jsx
+++ components/post/RelatedArticles.jsx
@@ -1,21 +1,58 @@
+import LargeImage from '../thumbnail/LargeImage'
+import NoImage from '../thumbnail/NoImage'
 
+import { connect } from 'react-redux'
 
+@connect(store => {
+  const topStories = store.collections.topStory
+  const featured = store.collections.featured
+  return {
+    featured,
+    topStories
+  }
+})
+class RelatedArticles extends React.Component {
+  render() {
+    const topStories = this.props.topStories ? this.props.topStories.slice(0,2) : []
+    const featured = this.props.featured ? this.props.featured : []
 
-const RelatedArticles = () => (
-  <div id="content">
-    <img src='/static/placeholder.png'></img>
-    <p>Related Article!!! Woohoo!!!</p>
-    <img src='/static/placeholder.png'></img>
-    <p>Related Article!!! Woohoo!!!</p>
-    <img src='/static/placeholder.png'></img>
-    <p>Related Article!!! Woohoo!!!</p>
-    <style jsx>{`
-      img {
-        width: 99%;
-      }
-    `}
-    </style>
-  </div>
-)
-
+    return (
+      <div id="container">
+        <div className="border-bottom border-dark" id="top-story">
+          {topStories.map(article => (
+            <div className="col-12" id="no-sum-large" key={article.slug}>
+              <ul>
+                <LargeImage article={article} withSummary={false} />
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className="border-top border-dark" id="no-image">
+          {featured.map(article => (
+            <div className="col-12" id="no-sum-large" key={article.slug}>
+              <ul>
+                <NoImage article={article} withSummary={false} />
+              </ul>
+            </div>
+          ))}
+        </div>
+        <style jsx>{`
+          ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+          }
+          #top-story {
+              padding-bottom: 5px;
+              margin-bottom: 10px;
+          }
+          #no-sum-large.ul {
+            margin: 10px;
+          }
+        `}
+        </style>
+      </div>
+    )
+  }
+}
 export default RelatedArticles
