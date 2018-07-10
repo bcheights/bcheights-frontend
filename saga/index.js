65b8e72a214127d84ad8292e81fb0d8c2cc72290
Branden Kim <brandenk0@gmail.com>
2018-07-09 22:50:33 -0700

    Refactor Redux-saga

--- saga/index.js
+++ saga/index.js
@@ -11,10 +11,10 @@ function* loadPost({ slug }) {
       type: "FETCH_POST_SUCCESS",
       payload: postData,
     })
-  } catch(err) {
+  } catch(error) {
     yield put({
       type: "FETCH_POST_FAILURE",
-      payload: err,
+      payload: error,
     })
   }
 }
@@ -27,14 +27,7 @@ function* loadCollection({ tagID }) {
       type: `FETCH_${actionType}_REQUEST`
     })
     const data = yield api.fetchCollection(tagID)
-    let collection = []
-    let parsedData = {}
-
-    // Parse each collection article
-    for (let i=0; i < data.length; i++) {
-      parsedData = yield api.parsePostData(data[i])
-      collection.push(parsedData)
-    }
+    const collection = yield parsePostsToArray(data)
     // Once finished --> Send success message
     yield put({
       type: `FETCH_${actionType}_SUCCESS`, 
@@ -43,7 +36,7 @@ function* loadCollection({ tagID }) {
   } catch(error) {
     yield put({
       type: `FETCH_${actionType}_FAILURE`, 
-      error
+      payload: error
     })
   }
 }
@@ -59,8 +52,36 @@ function getCollectionType(tagID) {
   }
 }
 
+function* loadSearch({ searchString }) {
+  try {
+    const data = yield api.fetchSearch(searchString)
+    const searchResults = yield parsePostsToArray(data)
+    yield put({
+      type: 'FETCH_SEARCH_SUCCESS',
+      payload: searchResults
+    })
+  } catch(error) {
+    yield put({
+      type: 'FETCH_SEARCH_FAILURE',
+      payload: error
+    })
+  }
+}
+
+function* parsePostsToArray(posts) {
+  let collection = []
+  let parsedData = {}
+
+  for (let i=0; i < posts.length; i++) {
+    parsedData = yield api.parsePostData(posts[i])
+    collection.push(parsedData)
+  }
+  return collection
+}
+
 // Watch for FETCH_FEATURED_REQUEST action and call loadFeatured
 export default function* rootSaga() {
   yield takeEvery('FETCH_COLLECTION_REQUEST', loadCollection)
   yield takeEvery('FETCH_POST_REQUEST', loadPost)
+  yield takeEvery('FETCH_SEARCH_REQUEST', loadSearch)
 }
