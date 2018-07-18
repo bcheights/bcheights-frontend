3c1c4c53943f5f5dafb4b3e570e949af90cca4f2
Branden Kim <brandenk0@gmail.com>
2018-07-17 19:59:46 -0700

    Add base component for magazine articles

---
+++ components/post/MagazineArticle.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Head from 'next/head'
+
+
+@connect(store => {
+  return {
+    title: store.post.title,
+    featured: store.post.featured,
+    author: store.post.author,
+    date: store.post.date,
+    content: store.post.content
+  }
+})
+class MagazineArticle extends React.Component {
+  render() {
+    const { title, author, date, featured, content } = this.props
+    return (
+      <div>
+        <img src={featured} />
+      </div>
+    )
+  }
+}
+
+export default connect()(MagazineArticle)
