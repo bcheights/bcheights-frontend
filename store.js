547d3e77e68355b7745f5483c8ba444a112fc2e5
Branden Kim <brandenk0@gmail.com>
2018-07-24 22:17:42 -0700

    Apply redux-devtools in store & dev dependencies only load in dev env

--- store.js
+++ store.js
@@ -1,13 +1,14 @@
 import { applyMiddleware, createStore } from "redux"
 import { logger } from "redux-logger"
 import createSagaMiddleware from "redux-saga"
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import reducer from "./reducers"
 import rootSaga from "./saga"
 
 const saga = createSagaMiddleware()
 const middleware = process.env.NODE_ENV === 'production' ? 
-    applyMiddleware(saga) : applyMiddleware(logger, saga);
+    applyMiddleware(saga) : composeWithDevTools(applyMiddleware(logger, saga));
 
 let store = null
 
