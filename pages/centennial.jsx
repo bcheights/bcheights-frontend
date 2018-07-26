ceefbf14f0ff4f94cb7e5ed70d7bb36bd8e64a66
Branden Kim <brandenk0@gmail.com>
2018-07-25 17:26:39 -0700

    Fix dumb mistake with centennial page

--- pages/centennial.jsx
+++ pages/centennial.jsx
@@ -8,21 +8,23 @@ import Footer from '../components/footer'
 
 class Centennial extends React.Component {
   render() {
-    <div>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-        <title>BC Heights - Centennial</title>
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" /> 
-      </Head>
+    return (
       <div>
-        <Header />
-        <Footer />
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+          <title>BC Heights - Centennial</title>
+
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" /> 
+        </Head>
+        <div>
+          <Header />
+          <Footer />
+        </div>
       </div>
-    </div>
+    )
   }
 }
 
