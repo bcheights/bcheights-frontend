204e7b4295f09998052cd084f55168c876f36345
Branden Kim <brandenk0@gmail.com>
2018-07-25 17:08:24 -0700

    Add category components

---
+++ components/category/Archive.jsx
@@ -0,0 +1,31 @@
+import LeftSideImage from '../thumbnail/LeftSideImage'
+
+import { connect } from 'react-redux'
+import React from 'react'
+
+
+@connect(store => {
+  return { posts: store.collections.category }
+})
+class CategoryArchive extends React.Component {
+  render() {
+    const posts = this.props.posts ? this.props.posts : []
+    return (
+      <div>
+        {posts.map(post => (
+          <LeftSideImage article={post} key={post.slug} withSummary={true}
+          border={true} isHeadline={true} />
+        ))}
+        <style jsx>{`
+          div {
+            margin: 0;
+            padding: 0;
+          }
+        `}
+        </style>
+      </div>
+    )
+  }
+}
+
+export default CategoryArchive
